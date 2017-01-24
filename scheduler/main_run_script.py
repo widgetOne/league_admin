cf09899a88d67222fa1568c58207dfc9e90ae3db
Brian Coulter <bcoulter@rentjungle.com>
2017-01-24 15:09:25 -0500

    spring 2017 updates
    a number of revisions to the controls
    to make this seasons schedules. for regular seaons, added logic to
    allow backup of play solutions seperate from potentially failing
    reffing solutions.

--- scheduler/main_run_script.py
+++ scheduler/main_run_script.py
@@ -8,8 +8,10 @@ from optimizer import make_round_robin_game, get_default_potential_sch_loc
 import datetime
 from facility import SCVL_Facility_Day
 from facility import Facility
+from pprint import pprint
 
 def make_regular_season(team_counts, ndays=9, sch_tries=500, seed=1):
+    """not currently used"""
     days = []
     import random
     random.seed(seed)
@@ -28,49 +30,48 @@ def make_regular_season_fall_2016():
     seed = 1
     print('\nMaking schedule %s.' % seed)
     sch = make_schedule(team_counts, fac,
-                        sch_tries=sch_tries, seed=seed, debug=True)
+                        sch_tries=sch_tries, seed=seed, debug=True, save_play_schedule=True)
     save_schedules([sch], canned_path)
-    audit_text = sch.get_audit_text()
-    print("\n".join(audit_text))
+    print(sch.get_audit_text())
 
-def make_round_robin_fall_2016():
-    team_counts = [6, 10, 11, 10, 6]
-    sch_template_path = 'test/Fall-2016-scrap-round_robin_csv_e.csv'
+
+def make_round_robin_spring_2017():
+    team_counts = [6, 12, 11, 10, 6]
+    sch_template_path = 'inputs/2017-spring/round_robin_template_2017-01-21a.csv'
     canned_path = 'test/scratch/'
-    total_schedules = 9205
+    total_schedules = 3000
     summary, schedules = make_round_robin_game(team_counts, sch_template_path, total_schedules)
-    choosing_a_winner = False
+    choosing_a_winner = True
     if choosing_a_winner:
-        # pick schedule and generate the csv reports
-        #sch = schedules[2047]
-        sch = summary['sitting is sitting <45']['sch']
-        audit_text = sch.get_audit_text()
-        print("\n".join(audit_text))
-        make_final_report = False
+        #pprint(summary)
+        sch = summary['min hour and no-sit']['sch']
+        print(sch)
+        make_final_report = True
         if make_final_report:
-            path = 'test/scratch/'
-            today = datetime.date.today()
-            file_name = '{}b_round_robin_sch.csv'.format(today)
-            file_path = path + file_name
+            file_path = 'scratch/{}b_round_robin_sch.csv'.format(datetime.date.today())
             sch.gen_csv(file_path)
 
 
 def make_regular_season_spring_2017():
-    team_counts = [6, 11, 11, 11, 6]
+    team_counts = [6, 12, 11, 10, 6]
     canned_path = get_default_potential_sch_loc(str(datetime.date.today()))
-    sch_template_path = 'inputs/reg_season/spring_2017_template.csv'
+    sch_template_path = 'inputs/reg_season/spring_2017_template_01_23b.csv'
     sch_tries = 5000
     fac = facility.sch_template_path_to_fac(sch_template_path, team_counts)
-    seed = 1
+    seed = 10  # 4 got through play
     print('\nMaking schedule %s.' % seed)
-    sch = make_schedule(team_counts, fac,
-                        sch_tries=sch_tries, seed=seed, debug=True)
+    sch = make_schedule(team_counts, fac, sch_tries=sch_tries, seed=seed, debug=True,
+                        save_play_schedule=True)
     save_schedules([sch], canned_path)
-    audit_text = sch.get_audit_text()
-    print("\n".join(audit_text))
+    print(sch.get_audit_text())
+    make_final_report = True
+    print(sch)
+    print(sch.solution_debug_data(1))
+    if make_final_report:
+        file_path = 'output/2017_spring/2017_spring_regular_season_sch_{}.csv'.format(datetime.date.today())
+        sch.gen_csv(file_path)
 
 
 if __name__ == '__main__':
-    make_regular_season_fall_2016()
-    #make_regular_season_spring_2017()
-
+    #make_regular_season_fall_2016()
+    make_regular_season_spring_2017()
