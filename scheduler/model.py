16ce710fc7490148a73fdbdbfcdc4aa249db1121
Brian Coulter <bcoulter@rentjungle.com>
2017-01-24 14:57:26 -0500

    updates for spring 2017
    enhanced the main game placement routine to
    explicitly account for who has played in a given day
    enhanced the ref placement logic to try both random shuffling
    in as well as specific ref duty transfers for high to low,
    cleaned up reffing finding logic to use simple set math,
    cleaned up ref printing logic to be dynamic instead of manual
    between regular season and round robin games.

--- scheduler/model.py
+++ scheduler/model.py
@@ -1,3 +1,5 @@
+from random import shuffle, choice
+from fitness import ScheduleFitness
 
 init_value = -999999
 
@@ -20,17 +22,18 @@ class Game(object):
         return out
 
 
-    def csv_str(self):
+    def csv_str(self, there_is_reffing=True):
         if self.div == init_value:
-            return "WARM UP  ,,"
+            if there_is_reffing:
+                return "WARM UP  ,   ,"
+            else:
+                return "WARM UP  ,"
         div_csv_str = ['REC', 'INT', 'COM', 'POW', 'P+ ', '']
         teams = [self.team1 + 1, self.team2 + 1]
-        out = "{} {}v{}".format(div_csv_str[self.div], min(teams), max(teams))
+        out = "{} {}v{},".format(div_csv_str[self.div], min(teams), max(teams))
         if self.ref > -1:
             ref_str = self.ref + 1
-        else:
-            ref_str = 'X'
-        out += ',' + div_csv_str[self.div] + " %s" % (ref_str) + ", "
+            out += div_csv_str[self.div] + " %s" % (ref_str) + ", "
         return out
 
 
@@ -63,8 +66,7 @@ class Day(object):
         return all_games
 
     def fitness_str(self):
-        from fitness import ScheduleFitness
-        return ScheduleFitness(self.facilities, self.games())
+        return ScheduleFitness(self.num, self.facilities, self.games())
 
     def isolated_new_fitness(self):
         return self.fitness_str().value()
@@ -114,15 +116,26 @@ class Day(object):
                     fitness -= double_use_penalty
         return fitness
 
+
+    def there_is_reffing(self):
+        for court in self.courts:
+            for game in court:
+                if game.ref > -1:
+                    return True
+        return False
+
     def csv_str(self):
         out = []
-        header = ",".join('CT '+ str(idx+1) + ',Ref' for idx in range(5))
+        if self.there_is_reffing():
+            header = ",".join('CT '+ str(idx+1) + ',Ref' for idx in range(5))
+        else:
+            header = ",".join('CT ' + str(idx + 1) for idx in range(5))
         out += [header]
         time_count = len(self.courts[0])
         for time in range(len(self.courts[0])):
             row = ""
             for court in range(len(self.courts)):
-                game_str = self.courts[court][time].csv_str()
+                game_str = self.courts[court][time].csv_str(there_is_reffing=self.there_is_reffing())
                 row += game_str
             out += [row]
         out += ["," * 2 * 5]
@@ -169,7 +182,7 @@ class Day(object):
             row = ""
             for court in range(len(self.courts)):
                 game = self.courts[court][time]
-                game_str = game.csv_str()
+                game_str = game.csv_str(there_is_reffing=self.there_is_reffing())
                 if (game.div >= 0):
                     if (game.ref >= 0):
                         total_use[game.div][game.ref] += 1
@@ -191,34 +204,40 @@ class Day(object):
                 if old_day.courts[court_idx][time_idx].div == div_idx:
                     self.courts[court_idx][time_idx] = deepcopy(game)
 
-    def schedule_div_players_then_refs(self, fac, div_idx, div):
-        from random import shuffle, choice
+    def schedule_div_players_without_refs(self, fac, div_idx, div):
         from schedule import list_filter
         game_slots = fac.div_games[div_idx].copy()
         shuffle(game_slots)
         day_idx = fac.day_idx
         # fill in players
+        teams_not_played_today = list(range(div.team_count))
         for court, time in game_slots:
             self.courts[court][time].div = div_idx
             # place team 1
             free = self.teams_free_at_time(div_idx, time, div.team_count)
             least_played = div.teams_w_least_play()
             best_list = free
+            best_list = list_filter(best_list, teams_not_played_today)
             best_list = list_filter(best_list, least_played)
             team1_idx = choice(best_list)
             self.courts[court][time].team1 = team1_idx
             div.add_play_for_team(team1_idx, day_idx)
+            if team1_idx in teams_not_played_today:
+                teams_not_played_today.remove(team1_idx)
             # place team 2
             free = self.teams_free_at_time(div_idx, time, div.team_count)
             team1 = div.teams[team1_idx]
             best_opponent = team1.teams_least_played()
             least_played = div.teams_w_least_play()
             best_list = free
-            best_list = list_filter(best_list, least_played)
             best_list = list_filter(best_list, best_opponent)
+            best_list = list_filter(best_list, teams_not_played_today)
+            best_list = list_filter(best_list, least_played)
             team2_idx = choice(best_list)
             self.courts[court][time].team2 = team2_idx
             div.add_play_for_team(team2_idx, day_idx, team1_idx)
+            if team2_idx in teams_not_played_today:
+                teams_not_played_today.remove(team2_idx)
 
     def teams_free_at_time(self, div_idx, time, team_count):
         free = {idx : 0 for idx in range(team_count)}
@@ -234,24 +253,40 @@ class Day(object):
         totals = {}
         return totals
 
-    def get_free_to_ref(self, div_idx, time):
-        potential_refs = set([])
-        for delta in [-1, 1]:
-            source_time = time + delta
-            if 0 <= source_time < self.facilities.time_count:
-                for court_idx in range(self.facilities.court_count):
-                    game = self.courts[court_idx][source_time]
-                    if game.div == div_idx:
-                        potential_refs.add(game.team1)
-                        potential_refs.add(game.team2)
-        for court_idx in range(self.facilities.court_count):
-            game = self.courts[court_idx][time]
-            if game.div == div_idx:
-                potential_refs.discard(game.team1)
-                potential_refs.discard(game.team2)
-                if game.ref > -1:
-                    potential_refs.discard(game.ref)
-        return list(potential_refs)
+    def get_free_to_ref(self, div_idx, target_time):
+        playing_before_or_after = set([])
+        playing_or_reffing_then = set([])
+        reffing_already_that_day = set([])
+        # First, add the teams from the games before and after
+        for court in self.courts:
+            for time, game in enumerate(court):
+                if game.div == div_idx:
+                    if game.ref > -1:
+                        reffing_already_that_day.add(game.ref)
+                    is_before_or_after_target = abs(target_time - time) == 1
+                    if is_before_or_after_target:
+                        playing_before_or_after.add(game.team1)
+                        playing_before_or_after.add(game.team2)
+                    elif target_time == time:
+                        playing_or_reffing_then.add(game.team1)
+                        playing_or_reffing_then.add(game.team2)
+                        if game.ref > -1:
+                            playing_or_reffing_then.add(game.ref)
+        should_ref = list(playing_before_or_after - playing_or_reffing_then - reffing_already_that_day)
+        can_ref = list(playing_before_or_after - playing_or_reffing_then)
+        if should_ref:
+            return should_ref
+        else:
+            if can_ref:
+                return can_ref
+            else:
+                return []
+                #raise(Exception('''Could not find a reffing option for division {} at time {} on day {}
+                #the total potential were {} but teama {} were playing at the same time and
+                #{} were reffing at other points in the day'''.format(div_idx, target_time, self.num,
+                #                                                     playing_before_or_after,
+                #                                                     playing_or_reffing_then,
+                #                                                     reffing_already_that_day)))
 
     def add_reffing(self, div_idx, div):
         from random import shuffle, choice
@@ -263,13 +298,28 @@ class Day(object):
         for court, time in game_slots:
             game = self.courts[court][time]
             ref_options = self.get_free_to_ref(div_idx, time)
-            if len(ref_options) == 0:
-                raise(Exception('we can\'t seem to find a ref for ' +
-                                'time = {}, court = {}, day = {}'.format(court, time, day_idx)))
-            best_ref_list = list_filter(ref_options, div.teams_w_least_ref())
+            teams_w_least_reffing = div.teams_w_least_ref()
+            best_ref_list = list_filter(ref_options, teams_w_least_reffing)
             reffing_team = choice(best_ref_list)
             game.ref = reffing_team
             div.teams[reffing_team].refs += 1
+
+
+    def try_transfer_reffing(self, from_list, to_list, div, div_idx):
+        for from_team in from_list:
+            for court_idx, court in enumerate(self.courts):
+                for time, game in enumerate(court):
+                    if game.div == div_idx and from_team == game.ref:
+                        free_to_ref = self.get_free_to_ref(div_idx, time)
+                        potential_to = list(set(to_list).intersection(set(free_to_ref)))
+                        if potential_to:
+                            new_ref_team = choice(potential_to)
+                            self.courts[court_idx][time].ref = new_ref_team
+                            div.teams[from_team].refs -= 1
+                            div.teams[new_ref_team].refs += 1
+                            from_list.remove(from_team)
+                            to_list.remove(new_ref_team)
+        return from_list, to_list, div
 
 
 class Division(object):
