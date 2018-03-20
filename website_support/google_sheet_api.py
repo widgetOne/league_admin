6cd6ef4aee491b5fb461f68f2282e132367ca2f5
Brian Coulter <bcoulter@rentjungle.com>
2018-03-20 11:43:10 -0400

    non-functional attempt a google sheet access
    I'm working remove the need for csvs in our workflow.
    the answer to that is using the googledrive / google sheets apis
    to pipe data between steps. This file is directly drawn from
    google sample pages, but is not functional yet for my purposes.
    I'm uploading here as I'm losing this computer.

---
+++ website_support/google_sheet_api.py
@@ -0,0 +1,107 @@
+from __future__ import print_function
+import httplib2
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from pprint import pprint
+
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    credential_path = 'google_sheets_auth.json'
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def get_service():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
+    service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
+    return service
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+
+    Creates a Sheets API service object and prints the names and majors of
+    students in a sample spreadsheet:
+    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+    """
+    service = get_service()
+    spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+    rangeName = 'Class Data!A2:E'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+
+
+def get_scvl_team_files():
+    """Shows basic usage of the Sheets API.
+
+    Creates a Sheets API service object and prints the names and majors of
+    students in a sample spreadsheet:
+    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+    """
+    service = get_service()
+    # result = service.activities().list(userId='me', collection='public').execute()
+    #result = service.activities().list(userId='me', collection='public').execute()
+    #result = service.spreadsheets().list(userId='me', collection='public').execute()
+    #result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
+    pprint(dir(service.spreadsheets()))
+    pprint(dir(service.spreadsheets().get()))
+    ''''batchUpdate', 'create', 'developerMetadata', 'get', 'getByDataFilter', 'sheets', 'values']'''
+    result = service.spreadsheets().list(spreadsheetId='me', collection='public').execute()
+    pprint(result)
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+
+
+if __name__ == '__main__':
+    get_scvl_team_files()
