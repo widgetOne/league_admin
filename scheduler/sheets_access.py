a11c4e19384f5b2bf4fac56fb443e60fe4f10cce
Brian Coulter <pittsburgh137@gmail.com>
2024-06-24 08:18:18 -0400

    Saving MVP of yaml config for gsheets
    There is some senative information (like the
    sheet with all the key teams information), that
    I'd like to keep out of git. So, I'm putting it
    all into a yaml config file I'm stowing in the
    auth .gitignore blind spot.

--- scheduler/sheets_access.py
+++ scheduler/sheets_access.py
@@ -1,13 +1,24 @@
 import os
+import yaml
 
 
 def get_auth_token():
     token_file_name = 'stonewall-volleyball-scheduler-gsheets-auth-token.json'
     token_path = os.path.join('../auth', token_file_name)
-
     with open(token_path, 'r') as token_file:
         return token_file.read()
 
 
+def get_sheet_config():
+    config_file_name = 'google_sheets_vball_config.yaml'
+
+
+def get_sheets_config():
+    config_file_name = 'gsheets_config.yaml'
+    config_path = os.path.join('../auth', config_file_name)
+    with open(config_path, 'r') as coonfig_file:
+        return coonfig_file.read()
+
+
 if __name__ == '__main__':
-    print(get_auth_token())
+    print(get_sheets_config())
