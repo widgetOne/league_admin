1e6802f1ee797e510f9cb6b1673477b6075efc49
Brian Coulter <pittsburgh137@gmail.com>
2024-06-24 08:16:16 -0400

    Initial structure of reffing splitting
    In Stonewall sand, reffing is a two-part activity where
    each reffing team provides two-line judges to 1 court
    and an up-ref to another.
    
    I'm adding post-processing logic to the solve to take the
    SCVL compatible, single-ref structure and adjust it to
    track both these refs. This is final transformation step
    before the score gets flattly formatted and pushed to the
    output trix.

--- scheduler/sheets_formatting.py
+++ scheduler/sheets_formatting.py
@@ -1,7 +1,52 @@
+import pandas as pd
 
-def main():
-    pass
-    
+import caching
+import schedule
+
+def split_out_reffing(sch):
+    for day in sch.days:
+        for time in range(len(day.courts[0])):
+            games = [day.courts[court][time]
+                     for court in range(len(day.courts))]
+            games = list(filter(lambda game: game.ref != schedule.init_value,
+                                games))
+            up_refs = [(game.div, game.ref) for game in games]
+            line_refs = up_refs[1:] + up_refs[:1]
+            new_refs = ((up, line) for up, line in zip(up_refs, line_refs))
+            for game, refs in zip(games, new_refs):
+                game.ref = refs
+    return sch
+
+
+def generate_sand_schedule_csv():
+    output_csv = ''
+    sch = caching.get_schedule_with_caching()
+    split_sch = split_out_reffing(sch)
+    for day in split_sch.days:
+        for court in day.courts:
+            for game in court:
+                print(game)
+
+
+def review_int_team_play_times():
+    int_teams = list(range(10))
+    df = pd.DataFrame(int_teams, index=int_teams)
+    all_times = ['12pm', '1pm', '2pm', '3pm', '4pm']
+    for time in all_times:
+        df[time] = 0
+    df.drop([0], axis=1, inplace=True)
+    sch = caching.get_schedule_with_caching()
+    for day in sch.days:
+        for court in day.courts:
+            times = all_times[-len(court):]
+            for game, time in zip(court, times):
+                if game.div == 1:
+                    df[time][game.team1] += 1
+                    df[time][game.team2] += 1
+    df = df.replace([0], "")
+    print(df)
+
 
 if __name__ == '__main__':
-    main()
+    #generate_sand_schedule_csv()
+    review_int_team_play_times()
