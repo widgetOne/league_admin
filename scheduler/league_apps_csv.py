dbfe7262764661b239e505e382b1fb68e0516a0d
Brian Coulter <pittsburgh137@gmail.com>
2024-06-27 11:11:49 -0400

    Final touchups on csv exporting: arg driven, refs
    I fixed the hours, which I had off by one. Need to
    make this dynamic from the template in the next
    iteration.
    
    Adding the line reffing duties in addition to the
    up reffing duties.
    
    Change the local run to be arg driven, so I can update the
    schedule being pushed into it.

--- scheduler/league_apps_csv.py
+++ scheduler/league_apps_csv.py
@@ -9,11 +9,12 @@ from schedule import init_value
 def make_league_apps_scv(sch):
     header_base = 'SUB_PROGRAM	HOME_TEAM	AWAY_TEAM	DATE	START_TIME	END_TIME	LOCATION	SUB_LOCATION	TYPE	NOTES'
     header = header_base.split('\t')
+    split_sch = sheets_formatting.split_out_reffing(sch)
     team_cypher = sheets_formatting.get_team_name_cypher()
     data = [header]
-    start_times = ['11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
+    start_times = ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
     day_strs = ['06/30/2024', '07/14/2024',  '07/28/2024',  '08/04/2024',  '08/11/2024', ]
-    for day_idx, day in enumerate(sch.days):
+    for day_idx, day in enumerate(split_sch.days):
         for time in range(len(day.courts[0])):
             for court_idx in range(len(day.courts)):
                 game = day.courts[court_idx][time]
@@ -29,7 +30,7 @@ def make_league_apps_scv(sch):
                     'Highland Park',
                     f'Court {court_idx+1}',
                     'REGULAR_SEASON',
-                    f'Up Ref {team_cypher[(game.div, game.ref)]}',
+                    f'Up Ref {team_cypher[game.ref[0]]}\nLine Ref {team_cypher[game.ref[1]]}',
                 ]
                 data.append(row)
     output_path = 'scratch/2024_sand_vball_league_apps.csv'
@@ -43,6 +44,7 @@ def make_league_apps_scv(sch):
             print(csv_str)
     else:
         sheets_access.set_league_apps_csv_sheet(data)
+        #print(data)
 
 
 def push_v2_schedule_everywhere():
@@ -53,4 +55,5 @@ def push_v2_schedule_everywhere():
 
 
 if __name__ == '__main__':
-    make_league_apps_scv()
+    sch = caching.load_v2_current_schedule()
+    make_league_apps_scv(sch)
