287ade504ebed147b42537b2b1ea2d36269bf4f3
Brian Coulter <pittsburgh137@gmail.com>
2024-06-27 11:12:16 -0400

    Adding the teamwise schedules to the bulk updated
    I'm missed this in the first pass.

--- scheduler/push_new_schedule_to_sheets.py
+++ scheduler/push_new_schedule_to_sheets.py
@@ -5,13 +5,14 @@ import sheets_formatting
 import csv
 from schedule import init_value
 import league_apps_csv
-
+import teamwise_schedules
 
 def push_v2_schedule_everywhere():
     sch = caching.load_v2_current_schedule()
 
     sheets_access.set_schedule_audit_sheet(sch.get_audit_text())
     league_apps_csv.make_league_apps_scv(sch)
+    teamwise_schedules.upload_teamwise_schedules(sch)
 
     schedule_list_list = sheets_formatting.format_sand_schedule(sch)
     sheets_access.set_formatted_schedule_to_sheet(schedule_list_list)
