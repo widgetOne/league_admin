45b46699e3bcaf72601560e6419f1513290d0fc5
Brian Coulter <pittsburgh137@gmail.com>
2024-06-27 08:08:05 -0400

    Adding some spacing for mental clarity in a short function

--- scheduler/push_new_schedule_to_sheets.py
+++ scheduler/push_new_schedule_to_sheets.py
@@ -9,8 +9,10 @@ import league_apps_csv
 
 def push_v2_schedule_everywhere():
     sch = caching.load_v2_current_schedule()
+
     sheets_access.set_schedule_audit_sheet(sch.get_audit_text())
     league_apps_csv.make_league_apps_scv(sch)
+
     schedule_list_list = sheets_formatting.format_sand_schedule(sch)
     sheets_access.set_formatted_schedule_to_sheet(schedule_list_list)
 
