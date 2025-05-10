ba8d503c83ac5185330bb6ea039fbc497b793928
Brian Coulter <pittsburgh137@gmail.com>
2025-05-10 10:40:12 -0400

    Adding addition and post_processors
    Adding the concept of post processors as I may need that for volleyball schedule cleaning, as well as the ability to add component, which I want for making default constrint blocks (basic games, basic softball, etc)

--- solver/schedule_component.py
+++ solver/schedule_component.py
@@ -7,6 +7,7 @@ class SchedulerComponent(object):
     def __init__(self):
         self._constraints = []
         self._optimizers = []
+        self._post_processors = []
 
     def add_constraint(self, constraint):
         """Add a constraint to the component."""
@@ -15,6 +16,10 @@ class SchedulerComponent(object):
     def add_optimizer(self, optimizer):
         """Add an optimizer to the component."""
         self._optimizers.append(optimizer)
+
+    def add_post_processor(self, post_processor):
+        """Add a post-processor to the component."""
+        self._post_processors.append(post_processor)
 
     def apply_constraints(self, schedule):
         """Apply all constraints to the schedule."""
@@ -25,6 +30,46 @@ class SchedulerComponent(object):
         """Apply all optimizers to the schedule."""
         for optimizer in self._optimizers:
             optimizer(schedule)
+
+    def post_process(self, schedule):
+        """Apply all post-processors to the schedule."""
+        for post_processor in self._post_processors:
+            post_processor(schedule)
+
+    def __add__(self, other):
+        """Combine two SchedulerComponents into a new one.
+        
+        Args:
+            other: Another SchedulerComponent to combine with
+            
+        Returns:
+            A new SchedulerComponent containing the combined elements
+        """
+        if not isinstance(other, SchedulerComponent):
+            raise TypeError("Can only combine SchedulerComponent with another SchedulerComponent")
+        
+        combined = SchedulerComponent()
+        combined._constraints = self._constraints + other._constraints
+        combined._optimizers = self._optimizers + other._optimizers
+        combined._post_processors = self._post_processors + other._post_processors
+        return combined
+
+    def __iadd__(self, other):
+        """In-place addition of another SchedulerComponent.
+        
+        Args:
+            other: Another SchedulerComponent to combine with
+            
+        Returns:
+            self with the other component's elements added
+        """
+        if not isinstance(other, SchedulerComponent):
+            raise TypeError("Can only combine SchedulerComponent with another SchedulerComponent")
+        
+        self._constraints.extend(other._constraints)
+        self._optimizers.extend(other._optimizers)
+        self._post_processors.extend(other._post_processors)
+        return self
 
 
 class EqualSeasonPlay(SchedulerComponent):
