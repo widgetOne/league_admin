a2365f33d2cb59ad89b3cc99ad33b8016ae55b3f
Brian Coulter <pittsburgh137@gmail.com>
2025-05-10 10:36:18 -0400

    cursor draft of the schedule component

--- solver/schedule_component.py
+++ solver/schedule_component.py
@@ -1,2 +1,66 @@
+class SchedulerComponent(object):
+    """Base class for all scheduling components.
+    
+    This class provides the foundation for different scheduling components
+    that can be combined to create a complete scheduling solution.
+    """
+    def __init__(self):
+        self._constraints = []
+        self._optimizers = []
 
+    def add_constraint(self, constraint):
+        """Add a constraint to the component."""
+        self._constraints.append(constraint)
+
+    def add_optimizer(self, optimizer):
+        """Add an optimizer to the component."""
+        self._optimizers.append(optimizer)
+
+    def apply_constraints(self, schedule):
+        """Apply all constraints to the schedule."""
+        for constraint in self._constraints:
+            constraint(schedule)
+
+    def optimize(self, schedule):
+        """Apply all optimizers to the schedule."""
+        for optimizer in self._optimizers:
+            optimizer(schedule)
+
+
+class EqualSeasonPlay(SchedulerComponent):
+    """Component that ensures equal number of games played per team."""
+    def __init__(self, total_season_play):
+        super().__init__()
+        self._constraints.append(self.generate_equal_season_play_constraints(total_season_play))
+
+    def generate_equal_season_play_constraints(self, total_season_play):
+        def apply_equal_play_constraint(schedule):
+            for team in schedule.teams:
+                if schedule.get_games_played(team) != total_season_play:
+                    raise ValueError(f"Team {team} has played {schedule.get_games_played(team)} games, expected {total_season_play}")
+        return apply_equal_play_constraint
+
+
+class SchedulerModel(SchedulerComponent):
+    """Base class for scheduling models."""
+    def __init__(self):
+        super().__init__()
+        self._model = None
+        self._team1 = {}
+        self._team2 = {}
+
+    def teams_by_divisions(self, team_counts):
+        """Organize teams into divisions based on team counts."""
+        pass
+
+
+class ReffedSchedulerModel(SchedulerModel):
+    """Scheduling model that includes referee assignments."""
+    def __init__(self):
+        super().__init__()
+        self._ref = {}
+
+    def assign_referees(self, schedule):
+        """Assign referees to games in the schedule."""
+        pass
 
