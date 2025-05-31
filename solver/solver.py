0b9203d551a0c6204fa4d6cf5aa73b5dbd3897df
Brian Coulter <pittsburgh137@gmail.com>
2025-05-31 16:08:49 -0400

    facility variables and constaint iterables
    adding the concept of applying facilities constaints to the model. I thikn I'll need to do this in a loop for all the games (which I don't thikn exiect currenlty), but am starting out with using it as a way to launder the games_per_season into the constriants without needing htem to understand the facilities ojbect.

--- solver/solver.py
+++ solver/solver.py
@@ -1,17 +1,17 @@
 from typing import List, Dict, Set, Any, Iterable
 from ortools.sat.python import cp_model
 from .facilities.facility import Facilities, GameSlot
-from .schedule_component.component import ScheduleComponent
+from .schedule_component import SchedulerComponent
 
 class SchedulerSolver:
     """A solver for scheduling games using constraint programming."""
     
-    def __init__(self, facilities: Facilities, constraints: Iterable[ScheduleComponent] = None, model=None):
+    def __init__(self, facilities: Facilities, constraints: Iterable[SchedulerComponent] = None, model=None):
         """Initialize the solver with facility constraints.
         
         Args:
             facilities: The Facilities object containing all facility constraints
-            constraints: Optional iterable of ScheduleComponents to apply
+            constraints: Optional iterable of SchedulerComponents to apply
             model: Optional OR-Tools model. If None, creates a new CpModel.
         """
         self.facilities = facilities
@@ -19,23 +19,39 @@ class SchedulerSolver:
         self.solver = cp_model.CpSolver()
         self.constraints = []
         
+        # Apply facility constraints to the model
+        self._apply_facilities_to_model()
+        
         if constraints:
             self.add_constraints(constraints)
         
-    def add_constraints(self, constraints: Iterable[ScheduleComponent]):
+    def _apply_facilities_to_model(self):
+        """Apply facility-level constraints to the model.
+        
+        This includes:
+        - Setting up games_per_season as a constant in the model
+        """
+        # Add games_per_season as a constant to the model
+        self.model.NewIntVar(
+            self.facilities.games_per_season,  # Lower bound
+            self.facilities.games_per_season,  # Upper bound
+            'games_per_season'  # Variable name
+        )
+        
+    def add_constraints(self, constraints: Iterable[SchedulerComponent]):
         """Add multiple constraints to the solver.
         
         Args:
-            constraints: Iterable of ScheduleComponents to add
+            constraints: Iterable of SchedulerComponents to add
         """
         for constraint in constraints:
             self.add_constraint(constraint)
         
-    def add_constraint(self, constraint: ScheduleComponent):
+    def add_constraint(self, constraint: SchedulerComponent):
         """Add a constraint to the solver.
         
         Args:
-            constraint: The ScheduleComponent to add
+            constraint: The SchedulerComponent to add
         """
         self.constraints.append(constraint)
         constraint.apply(self.model, self.facilities)
@@ -55,12 +71,12 @@ class SchedulerSolver:
 class ReffedSchedulerSolver(SchedulerSolver):
     """A solver for scheduling games that includes referee assignments."""
     
-    def __init__(self, facilities: Facilities, constraints: Iterable[ScheduleComponent] = None, model=None):
+    def __init__(self, facilities: Facilities, constraints: Iterable[SchedulerComponent] = None, model=None):
         """Initialize the solver with facility and referee constraints.
         
         Args:
             facilities: The Facilities object containing all facility constraints
-            constraints: Optional iterable of ScheduleComponents to apply
+            constraints: Optional iterable of SchedulerComponents to apply
             model: Optional OR-Tools model. If None, creates a new CpModel.
         """
         super().__init__(facilities, constraints, model)
