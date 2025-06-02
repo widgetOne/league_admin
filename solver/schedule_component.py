b517fcb48c5c321022b2c08f48630a4a4da7b8f8
Brian Coulter <pittsburgh137@gmail.com>
2025-06-02 17:52:24 -0400

    Fixing commenting error

--- solver/schedule_component.py
+++ solver/schedule_component.py
@@ -4,19 +4,19 @@ from ortools.sat.python import cp_model
 from .facilities.facility import Facilities
 
 class ModelActor(ABC):
-    """Base class for any object that can act on the model."""
+    """Base class for any object that can act on the model.
     
     These are function which act on the schedule model based on
     concrete realities from the facilities object.
     """
-        def __init__(self, actor_function: Callable[[cp_model.CpModel, Facilities], None]):
+    def __init__(self, actor_function: Callable[[cp_model.CpModel, Facilities], None]):
         """Initialize with a function that acts on the model.
         
         Args:
             actor_function: Function that takes a model and facilities, then acts on the model
         """
         self._actor_function = actor_function
-        
+
     def __call__(self, model: cp_model.CpModel, facilities: Facilities):
         """Call the actor function with the given model and facilities."""
         self._actor_function(model, facilities)
@@ -27,14 +27,13 @@ class SchedulerComponent:
     A component can add constraints, optimizers, validators, or post-processors
     to the scheduling model.
     """
-    
     def __init__(self):
         """Initialize empty lists for each type of manipulator."""
         self._constraints: List[ModelActor] = []
         self._optimizers: List[ModelActor] = []
         self._validators: List[ModelActor] = []
         self._post_processors: List[ModelActor] = []
-        
+
     def add_constraint(self, constraint: ModelActor):
         """Add a constraint to the component.
         
@@ -42,7 +41,7 @@ class SchedulerComponent:
             constraint: The constraint to add
         """
         self._constraints.append(constraint)
-        
+
     def add_optimizer(self, optimizer: ModelActor):
         """Add an optimizer to the component.
         
@@ -50,7 +49,7 @@ class SchedulerComponent:
             optimizer: The optimizer to add
         """
         self._optimizers.append(optimizer)
-        
+
     def add_validator(self, validator: ModelActor):
         """Add a validator to the component.
         
@@ -58,7 +57,7 @@ class SchedulerComponent:
             validator: The validator to add
         """
         self._validators.append(validator)
-        
+
     def add_post_processor(self, post_processor: ModelActor):
         """Add a post-processor to the component.
         
@@ -66,13 +65,13 @@ class SchedulerComponent:
             post_processor: The post-processor to add
         """
         self._post_processors.append(post_processor)
-        
+
     def __add__(self, other: 'SchedulerComponent') -> 'SchedulerComponent':
         """Combine two components.
         
         Args:
             other: Another component to combine with
-            
+        
         Returns:
             A new component containing all manipulators from both components
         """
@@ -82,13 +81,13 @@ class SchedulerComponent:
         result._validators = self._validators + other._validators
         result._post_processors = self._post_processors + other._post_processors
         return result
-        
+
     def __iadd__(self, other: 'SchedulerComponent') -> 'SchedulerComponent':
         """Add another component to this one in place.
         
         Args:
             other: Another component to add
-            
+        
         Returns:
             self, with the other component's manipulators added
         """
@@ -97,7 +96,6 @@ class SchedulerComponent:
         self._validators.extend(other._validators)
         self._post_processors.extend(other._post_processors)
         return self
-
 
 class EqualSeasonPlay(SchedulerComponent):
     """Component that ensures equal number of games played per team."""
