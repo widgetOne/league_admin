577cd58793d6956a61b800a5f1a15a9c394488c4
Brian Coulter <pittsburgh137@gmail.com>
2025-06-02 21:29:36 -0400

    Fixing imports assist in init

--- solver/__init__.py
+++ solver/__init__.py
@@ -1,4 +1,14 @@
 from .schedule import Schedule, ReffedSchedule
-from .facilities.facility import Facilities, GameSlot, TimeSlot
+from .facilities.facility import Facilities, Match, TimeSlot
+from .schedule_component import SchedulerComponent
+from .schedule_interface import ScheduleInterface
 
-__all__ = ['Schedule', 'ReffedSchedule', 'Facilities', 'GameSlot', 'TimeSlot']
+__all__ = [
+    'Schedule', 
+    'ReffedSchedule', 
+    'Facilities', 
+    'Match',
+    'TimeSlot',
+    'SchedulerComponent',
+    'ScheduleInterface'
+]
