db1d99922ec1f5ba0cf8291a3d328fa060e95c7b
Brian Coulter <pittsburgh137@gmail.com>
2025-06-05 17:54:30 -0400

    Adding vs play to the generic sand volleyball constrits

--- solver/component_sets/sand_volleyball_template.py
+++ solver/component_sets/sand_volleyball_template.py
@@ -1,4 +1,5 @@
 from ..components.total_play import TotalPlayConstraint
+from ..components.vs_play_balance import VsPlayBalanceConstraint
 
 def get_sand_volleyball_template():
     """Get the template of components for sand volleyball scheduling.
@@ -7,4 +8,4 @@ def get_sand_volleyball_template():
        Returns a list of the constaints required by default for any
        sand volleyball schedule.
     """
-    return [TotalPlayConstraint()]
+    return [TotalPlayConstraint(), VsPlayBalanceConstraint()]
