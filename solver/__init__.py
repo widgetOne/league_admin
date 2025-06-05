ec585f0e8cb0e1ccfebed6e72f60ce9e2b64339e
Brian Coulter <pittsburgh137@gmail.com>
2025-06-04 20:09:38 -0400

    Updating to schedule creator addition

--- solver/__init__.py
+++ solver/__init__.py
@@ -1,14 +1,14 @@
 from .schedule import Schedule, ReffedSchedule
+from .schedule_creator import ScheduleCreator
 from .facilities.facility import Facilities, Match, TimeSlot
 from .schedule_component import SchedulerComponent
-from .schedule_interface import ScheduleInterface
 
 __all__ = [
     'Schedule', 
     'ReffedSchedule', 
+    'ScheduleCreator',
     'Facilities', 
     'Match',
     'TimeSlot',
-    'SchedulerComponent',
-    'ScheduleInterface'
+    'SchedulerComponent'
 ]
