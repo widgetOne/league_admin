f73b1c4ecf8797aab2a909a291bf8d055861e1a3
Brian Coulter <pittsburgh137@gmail.com>
2026-05-16 20:55:07 -0400

    updating interdivision reffing rules in default template

--- solver/component_sets/sand_volleyball_template.py
+++ solver/component_sets/sand_volleyball_template.py
@@ -3,6 +3,8 @@ from ..components.vs_play_balance import VsPlayBalanceConstraint
 from ..components.balance_reffing import BalanceReffingConstraint
 from ..components.play_near_ref import PlayNearRefConstraint
 from ..components.ref_same_division import RefSameDivisionConstraint
+from ..components.ref_comp_division import CompRefCompConstraint
+from ..components.same_div_ref_optimization import SameDivisionRefOptimization
 from ..components.one_thing_at_a_time import OneThingAtATimeConstraint
 from ..components.rec_in_low_courts import RecInLowCourtsProcessor
 from ..components.time_variety_optimization import TimeVarietyOptimization
@@ -21,9 +23,11 @@ def get_sand_volleyball_template():
         BalanceReffingConstraint(),
         OneThingAtATimeConstraint(),
         PlayNearRefConstraint(),
-        RefSameDivisionConstraint(),
+        CompRefCompConstraint(),  # Only enforces same-div refs for the competitive division
+        SameDivisionRefOptimization(weight=10.0),  # Soft penalty per cross-division ref
         TimeVarietyOptimization(weight=1.0),
         RecInLowCourtsProcessor(),
         ByeWeekOptimization(weight=10000.0),
         NoThreeHoursDays(),
     ]
+
